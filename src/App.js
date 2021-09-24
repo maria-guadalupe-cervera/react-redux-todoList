20ee5a18dc525e17c7c64cd73767548c7627ed70
maria-guadalupe-cervera <86074558+maria-guadalupe-cervera@users.noreply.github.com>
2021-09-24 14:01:03 -0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import React from 'react';
+
+
+import './App.css';
+import Input from './components/input';
+import { TodoItem } from './components/TodoItem';
+import{useSelector}from 'react-redux';
+import{selectTodoList}from'./features/todoSlice'
+
+function App() {
+  const todoList=useSelector(selectTodoList)
+  return (
+    <div className="app">
+      <div className="app_container">
+        <div className="app_todoContainer">
+        {
+          todoList.map(item=>(
+            <TodoItem name={item.item}
+            done={item.done}
+            id={item.id}
+            />
+          ))
+        }
+        </div>
+        <Input/>
+      </div>
+    </div>
+  );
+}
+
+export default App;
